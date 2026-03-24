70e7b718f3135d221299244c2bda9af02aa10f7a
GiulianoVV <giuliano@managerinfo.com.br>
2026-03-23 23:09:07 -0300

    cd feat: initialize React project with Vite, ESLint, and basic stylingnpw
    - Add package.json with dependencies and scripts for development and build
    - Create App component with basic structure and styling
    - Add global CSS variables and styles for light and dark themes
    - Implement Vite configuration for React
    - Include App.css for component-specific styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <div> 
         <h1 className='forma'>Olá Mundo!</h1>
-        <p>Meu primeiro React</p>
+        <p>Meu primeiro React com git</p>
       </div>
     </>
   )

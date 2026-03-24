534ee4baff93cf5b1fc8052f9f9d520364468602
GiulianoVV <giuliano@managerinfo.com.br>
2026-03-23 21:15:09 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div> 
+        <h1 className='forma'>Olá Mundo!</h1>
+        <p>Meu primeiro React</p>
+      </div>
+    </>
+  )
+}
+
+export default App
